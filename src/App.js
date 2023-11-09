ab638bce1682c84d2db85b61ad4ca256d7ff9cd9
botmi2510 <kudotruong2002@gmail.com>
2023-11-09 10:49:34 +0700

    change file in reactJsHook

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Try your best!</h1>
+        <h3>Hello World!</h3>
       </header>
     </div>
   );
