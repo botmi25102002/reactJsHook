441cc96ac456567d53c9f184428004e2b629ef2b
botmi2510 <kudotruong2002@gmail.com>
2023-11-10 23:08:15 +0700

    thanh Nav

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Nav from "./views/Nav";
 
 const App = () => {
   let name = "Truong";
   let age = 21;
   let obj = { name: "Truong", age: "21" };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>
-          Hello {obj.name} - {obj.age}
-        </h3>
-        <p>{JSON.stringify(obj)}</p>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Nav />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h3>
+            Hello {obj.name} - {obj.age}
+          </h3>
+          <p>{JSON.stringify(obj)}</p>
+        </header>
+      </div>
+    </>
   );
 };
 
