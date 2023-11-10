71daa1970a33ee1ad6525a6b50ac18cbd9f6db7c
botmi2510 <kudotruong2002@gmail.com>
2023-11-10 15:46:57 +0700

    du lieu cua bien

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
+  let name = "Truong";
+  let age = 21;
+  let obj = { name: "Truong", age: "21" };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>Hello World!</h3>
+        <h3>
+          Hello {obj.name} - {obj.age}
+        </h3>
+        <p>{JSON.stringify(obj)}</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
